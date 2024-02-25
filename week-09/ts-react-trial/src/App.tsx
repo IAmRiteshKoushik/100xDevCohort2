a2b5899f21286a10b0813ddf6a596789546f1d59
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-02-25 14:50:17 +0530

    (check) Writing ts inside React

---
+++ week-09/ts-react-trial/src/App.tsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <Todo
+            title="Hi There"
+            description="ask"
+            done={false}
+        />
+    </>
+  )
+}
+
+interface TodoProp {
+    title: string,
+    description: string,
+    done: boolean,
+}
+
+function Todo(props: TodoProp) {
+    return(
+        <div>
+            <h1>{props.title}</h1>
+            <h2>{props.description}</h2>
+        </div>
+    )
+}
+
+export default App
