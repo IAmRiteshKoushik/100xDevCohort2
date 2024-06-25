9744823bb6e51e6ca73291a9e08ebec511bd04a5
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-06-26 00:41:39 +0530

    Passing children as components in React

--- week-06/week-06-exp/src/App.jsx
+++ week-06/week-06-exp/src/App.jsx
@@ -1,73 +1,50 @@
-import { useState } from "react";
-
 function App(){
-    const[todos, setTodos] = useState([
-        {
-            id: 1,
-            title: "Go to gym",
-            desc: "Go to gym today",
-        },
-        {
-            id: 2,
-            title: "Go to cafe",
-            desc: "Go to cafe todayA,"
-        },
-        {
-            id: 3,
-            title: "Go to library",
-            desc: "Go to lib todayA,"
-        },
-    ]);
-
-    function addTodo(){
-        setTodos([...todos, {
-            id: 4,
-            title: Math.random(),
-            description: Math.random(),
-        }]);
-    }
-
-    // Alternate way to write the previous function (syntax is less overwhelming)
-    // UGLIER DEFINITELY
-    function altAddTodos(){
-        const newTodos = [];
-        for(let i = 0; i < todos.length; i++){
-            newTodos.push(todos[i]);
-        }
-        newTodos.push({
-            id: 4,
-            title: Math.random(),
-            description: Math.random(),
-        });
-        setTodos(newTodos);
-    }
-
-    // Usage of Keys:
-    // ------------------------------------------------------------------------
-    // When using structures like arrays, when an array is re-ordered / sorted 
-    // manipulated in any capacity then keys helps react to avoid re-rendering the 
-    // entire list of components that were uniquely created with the elements of 
-    // the list and instead of just changing the structure or ordering of components
     return(
-        <div>
-            <button onClick={addTodo}>Add a todo</button>
-            {
-                todos.map((todo) => (
-                    <Todo 
-                        title={todo.title} 
-                        description={todo.description}
-                        key={todo.id}
-                    />
-                ))}
-        </div>
+        <>
+            <CardWrapper>
+                hi there
+            </CardWrapper>
+            <CardWrapper>
+                <div>
+                    hi there22
+                </div>
+            </CardWrapper>
+        </>
     );
 }
 
-function Todo({title, description}){
+// One way to do it, but it is not done this way. The cleaner 
+// syntax to do this would be as follows:
+// function CardWrapper({innerComponent}){
+//     return(
+//         <div style={{border: "2px solid black", padding: 20}}>
+//             {innerComponent} 
+//         </div>
+//     ); 
+// }
+//
+// function TextComponent2(){
+//     return(
+//         <div>
+//             hi there22
+//         </div>
+//     );
+// }
+// function TextComponent(){
+//     return(
+//         <div>
+//             hi there
+//         </div>
+//     );
+// }
+
+function CardWrapper({children}){
+    // children is a special prop which gives access to all the tags or text 
+    // put in between the opening and closing tags
+    console.log(children);
     return(
-        <div>
-            <h1>{title}</h1>
-            <p>{description}</p>
+        <div style={{border: "2px solid black", padding: 20}}>
+            {children}
         </div>
     );
 }
